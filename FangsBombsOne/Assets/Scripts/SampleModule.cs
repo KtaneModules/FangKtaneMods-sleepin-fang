3eaf8d1837c93ebb8848f4ed584d83306f6c62ba
Riverbui <4911928+rocket0634@users.noreply.github.com>
2020-08-04 09:52:15 -0700

    Fang's Random Bombs recovery

---
+++ FangsBombsOne/Assets/Scripts/SampleModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleModule : MonoBehaviour {
+
+    private static int _moduleIDCounter = 1;
+    private int _moduleID;
+    public KMBombInfo Info;
+    public KMBombModule Module;
+    public KMAudio Audio;
+    private bool active;
+
+    void Start () {
+        _moduleID = _moduleIDCounter++;
+        Module.OnActivate += delegate () { active = true; };
+    }
+}
